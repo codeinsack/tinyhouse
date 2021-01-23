835cac1a1edcca54ac564c94e4036a12f18205ed
codeinsack <codeinsack@gmail.com>
2021-01-23 18:11:21 +0300

    add resolvers and typeDefs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 
-import { schema } from './graphql';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
 const PORT = 9000;
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen(PORT);
