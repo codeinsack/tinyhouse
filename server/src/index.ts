24b5efd4c419da1a700c196bcb3d86eea43aabdb
codeinsack <codeinsack@gmail.com>
2021-01-23 17:29:38 +0300

    add apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,32 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+
+import { schema } from './graphql';
 
 const app = express();
-
 const PORT = 9000;
 
-app.use(bodyParser.json());
-
-const one = 1;
-const two = 2;
-
-app.get('/', (req, res) => res.send(`1 + 2 = ${one + two}`));
-
-app.get('/listings', (req, res) => {
-  return res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('Failed to delete listing');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(PORT);
