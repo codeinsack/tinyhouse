9d1d2376cfae131f8fa362d1143ffe1d8d39325e
codeinsack <codeinsack@gmail.com>
2021-03-01 23:46:17 +0300

    implement google auth on client side

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Layout } from "antd";
@@ -14,12 +14,24 @@ import {
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import "./styles/index.css";
+import { Viewer } from "./lib/types";
 
 const client = new ApolloClient({
   uri: "/api",
 });
 
+const initialViewer: Viewer = {
+  id: null,
+  token: null,
+  avatar: null,
+  hasWallet: null,
+  didRequest: false,
+};
+
 const App = () => {
+  const [viewer, setViewer] = useState<Viewer>(initialViewer);
+  console.log("viewer", viewer);
+
   return (
     <Router>
       <Layout id="app">
@@ -28,7 +40,11 @@ const App = () => {
           <Route exact path="/host" component={Host} />
           <Route exact path="/listing/:id" component={Listing} />
           <Route exact path="/listings/:location" component={Listings} />
-          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/login"
+            render={(props) => <Login {...props} setViewer={setViewer} />}
+          />
           <Route exact path="/user/:id" component={User} />
           <Route component={NotFound} />
         </Switch>
