c65cc1a4d318f7c0772c93798bc5ff58bce58c10
codeinsack <codeinsack@gmail.com>
2021-03-02 23:08:29 +0300

    handle X-CSRF-TOKEN

--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -32,8 +32,9 @@ export const Login = ({ setViewer }: Props) => {
     { data: logInData, loading: logInLoading, error: logInError },
   ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
     onCompleted: (data) => {
-      if (data?.logIn) {
+      if (data?.logIn && data.logIn.token) {
         setViewer(data.logIn);
+        sessionStorage.setItem("token", data.logIn.token);
         displaySuccessNotification("You've successfully logged in!");
       }
     },
