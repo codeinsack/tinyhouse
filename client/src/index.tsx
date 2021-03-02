eb2b0609e6fcd3ef12d22616c0fd22ffe031290f
codeinsack <codeinsack@gmail.com>
2021-03-02 21:52:31 +0300

    implement logout mutation

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -31,13 +31,12 @@ const initialViewer: Viewer = {
 
 const App = () => {
   const [viewer, setViewer] = useState<Viewer>(initialViewer);
-  console.log("viewer", viewer);
 
   return (
     <Router>
       <Layout id="app">
         <Affix offsetTop={0} className="app__affix-header">
-          <AppHeader viewer={viewer} />
+          <AppHeader viewer={viewer} setViewer={setViewer} />
         </Affix>
         <Switch>
           <Route exact path="/" component={Home} />
--- client/src/sections/AppHeader/components/MenuItems/index.tsx
+++ client/src/sections/AppHeader/components/MenuItems/index.tsx
@@ -3,23 +3,53 @@ import { Button, Menu, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import { HomeOutlined, UserOutlined, LogoutOutlined } from "@ant-design/icons";
 import { Viewer } from "../../../../lib/types";
+import { useMutation } from "@apollo/react-hooks";
+import { LogOut as LogOutData } from "../../../../lib/graphql/mutations/LogOut/__generated__/LogOut";
+import { LOG_OUT } from "../../../../lib/graphql/mutations";
+import {
+  displayErrorMessage,
+  displaySuccessNotification,
+} from "../../../../lib/utils";
 
 interface Props {
   viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
 }
 
 const { Item, SubMenu } = Menu;
 
-export const MenuItems = ({ viewer }: Props) => {
+export const MenuItems = ({ viewer, setViewer }: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: (data) => {
+      if (data?.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification("You've successfully logged out!");
+      }
+    },
+    onError: () => {
+      displayErrorMessage(
+        "Sorry! We weren't able to log you out. Please try again later!"
+      );
+    },
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  };
+
   const subMenuLogin = viewer.id ? (
     <SubMenu title={<Avatar src={viewer.avatar} />}>
       <Item key="/user">
-        <UserOutlined />
-        Profile
+        <Link to={`/user/${viewer.id}`}>
+          <UserOutlined />
+          Profile
+        </Link>
       </Item>
       <Item key="/logout">
-        <LogoutOutlined />
-        Logout
+        <div onClick={handleLogOut}>
+          <LogoutOutlined />
+          Logout
+        </div>
       </Item>
     </SubMenu>
   ) : (
