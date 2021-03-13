4ff634aa59a2a0ba8f9e4b8a15047c7fb663e1cb
codeinsack <codeinsack@gmail.com>
2021-03-13 14:33:28 +0300

    add latest listings on home page

--- client/src/sections/Home/index.tsx
+++ client/src/sections/Home/index.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link, RouteComponentProps } from "react-router-dom";
 import { Layout, Typography, Col, Row } from "antd";
-import { HomeHero } from "./components";
+import { HomeHero, HomeListings, HomeListingsSkeleton } from "./components";
 
 import mapBackground from "./assets/map-background.jpg";
 import sanFransiscoImage from "./assets/san-fransisco.jpg";
 import cancunImage from "./assets/cancun.jpg";
 import { displayErrorMessage } from "../../lib/utils";
+import { useQuery } from "react-apollo";
+import {
+  Listings,
+  ListingsVariables,
+} from "../../lib/graphql/queries/Listings/__generated__/Listings";
+import { LISTINGS } from "../../lib/graphql/queries";
+import { ListingsFilter } from "../../lib/graphql/globalTypes";
 const { Content } = Layout;
 const { Paragraph, Title } = Typography;
 
+const PAGE_LIMIT = 4;
+const PAGE_NUMBER = 1;
+
 export const Home = ({ history }: RouteComponentProps) => {
+  const { loading, data } = useQuery<Listings, ListingsVariables>(LISTINGS, {
+    variables: {
+      filter: ListingsFilter.PRICE_HIGH_TO_LOW,
+      limit: PAGE_LIMIT,
+      page: PAGE_NUMBER,
+    },
+  });
+
   const onSearch = (value: string) => {
     const trimmedValue = value.trim();
     if (trimmedValue) {
@@ -18,6 +36,23 @@ export const Home = ({ history }: RouteComponentProps) => {
     } else {
       displayErrorMessage("Please enter a valid search!");
     }
+  };
+
+  const renderListingsSection = () => {
+    if (loading) {
+      return <HomeListingsSkeleton />;
+    }
+
+    if (data) {
+      return (
+        <HomeListings
+          title="Premium Listings"
+          listings={data.listings.result}
+        />
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -41,7 +76,10 @@ export const Home = ({ history }: RouteComponentProps) => {
           Popular listings in the United States
         </Link>
       </div>
-      <div>
+
+      {renderListingsSection()}
+
+      <div className="home__listings">
         <Title level={4} className="home__listings-title">
           Listings of any kind
         </Title>
